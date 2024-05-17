0e10d01b968c89e79a62999e8b142d11e193b903
at0mchik <113366981+at0mchik@users.noreply.github.com>
2024-05-17 19:02:09 +0300

    help

--- atom/src/main/java/passstorage/atom/AtomApplication.java
+++ atom/src/main/java/passstorage/atom/AtomApplication.java
@@ -2,6 +2,11 @@ package passstorage.atom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+// import jakarta.activation.DataSource;
 
 @SpringBootApplication
 public class AtomApplication {
@@ -9,5 +14,22 @@ public class AtomApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AtomApplication.class, args);
 	}
+
+	// @Bean
+    // public DataSource dataSource(){
+    //     System.out.println(driverClass+" "+ url+" "+username+" "+password);
+    //     DriverManagerDataSource source = new DriverManagerDataSource();
+    //     source.setDriverClassName(driverClass);
+    //     source.setUrl(url);
+    //     source.setUsername(username);
+    //     source.setPassword(password);
+    //     return source;
+    // }
+
+    // @Bean
+    // public NamedParameterJdbcTemplate namedParameterJdbcTemplate(){
+    //     NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(this.dataSource());
+    //     return namedParameterJdbcTemplate;
+    // }
 
 }
